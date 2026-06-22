0468501939653ed5dca274e13a524bd8588ffaee
kammini <164534632+kammini@users.noreply.github.com>
2026-06-22 21:37:14 +0200

    implement file extraction

--- backend/app/core/extract.py
+++ backend/app/core/extract.py
@@ -1,5 +1,21 @@
-from fastapi import UploadFile
 
-async def extract_file(file: UploadFile):
-    content = await file.read()
-    print(content)
+from pypdf import PdfReader
+import docx
+
+async def extract_document(filepath: str) -> str:
+    if filepath.endswith(".pdf"):
+        reader = PdfReader(filepath)
+        text = "\n".join(page.extract_text() for page in reader.pages)
+        return text
+    
+    elif filepath.endswith(".docx"):
+        doc = docx.Document(filepath)
+        text = "\n".join(p.text for p in doc.paragraphs)
+        return text
+    
+    elif filepath.endswith(".txt", ".md"):
+        with open(filepath, "r", encoding="UTF-8") as f:
+            return f.read()
+        
+    else:
+        raise ValueError(f"Unsupported file type: {filepath}")
